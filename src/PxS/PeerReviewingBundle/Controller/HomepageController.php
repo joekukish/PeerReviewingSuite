6bd4911648547bd96e1c6d0cc067ff88008b7167
Oscar Bolaños <orbolanos@gmail.com>
2012-03-12 22:44:38 +0100

    Revert "Revert "Started templates of user part""
    This reverts commit bb84b949d2f7a23d5c72911a29ec0d36b074a4d8.

--- src/PxS/PeerReviewingBundle/Controller/HomepageController.php
+++ src/PxS/PeerReviewingBundle/Controller/HomepageController.php
@@ -5,11 +5,11 @@ namespace PxS\PeerReviewingBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
-class DefaultController extends Controller
+class HomepageController extends Controller
 {
-    
-    public function indexAction($name)
-    {
-        return $this->render('PxSPeerReviewingBundle:Homepage:index.html.twig', array('name' => $name));
-    }
+
+	public function indexAction()
+	{
+		return $this->render('PxSPeerReviewingBundle:Homepage:index.html.twig', array('name' => 'hola'));
+	}
 }
