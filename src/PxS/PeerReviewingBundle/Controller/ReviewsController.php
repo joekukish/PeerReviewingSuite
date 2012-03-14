baeb3f5144e4b17b640cb543c503dedc245b9cf0
Oscar Bolaños <orbolanos@gmail.com>
2012-03-14 07:51:12 +0100

    Integrated comments
    Added values into the DB.
    Detail page implemented.
    Included star-rating component.

--- src/PxS/PeerReviewingBundle/Controller/ReviewsController.php
+++ src/PxS/PeerReviewingBundle/Controller/ReviewsController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ReviewsController extends Controller
 {
-    public function indexAction($user)
+    public function reviewsAction($user)
     {
     	$user = $this->getDoctrine()->getRepository('PxSPeerReviewingBundle:User')
     			->find($user);
@@ -17,5 +17,31 @@ class ReviewsController extends Controller
 //    		->findBy(array(), array('timestamp'=>'asc'));
 	    
         return $this->render('PxSPeerReviewingBundle:Reviews:index.html.twig', array('page'=>'reviews', 'user' => $user, 'reviews' => $user->getReviews()));
+    }
+    
+    public function peerAction($user)
+    {
+    	$user = $this->getDoctrine()->getRepository('PxSPeerReviewingBundle:User')
+    			->find($user);
+
+//        $reviews = $this->getDoctrine()
+//    		->getRepository('PxSPeerReviewingBundle:Review')
+//    		->findBy(array(), array('timestamp'=>'asc'));
+	    
+        return $this->render('PxSPeerReviewingBundle:Reviews:index.html.twig', array('page'=>'peer-reviews', 'user' => $user, 'reviews' => $user->getPresentations()));
+    }    
+    
+    public function detailAction($user, $review)
+    {
+        $user = $this->getDoctrine()->getRepository('PxSPeerReviewingBundle:User')
+    			->find($user);
+    			
+		$review = $this->getDoctrine()->getRepository('PxSPeerReviewingBundle:Review')
+    			->find($review);
+
+		$page = $review->getReviewer()->getId() == $user->getId()? 'reviews':'peer-reviews';
+    			
+		
+		return $this->render('PxSPeerReviewingBundle:Reviews:detail.html.twig', array('page'=>$page, 'user' => $user, 'review' => $review));
     }
 }
